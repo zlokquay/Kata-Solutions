44f0c9a5a02832d3daa280848860deaa3e0d5245
Zachary Taylor <35826908+zlokquay@users.noreply.github.com>
2018-02-03 13:31:33 -0500

    Create Mumbling.js
    Solution for Mumbling.

---
+++ Mumbling.js
@@ -0,0 +1,20 @@
+const accum = (s) => {
+  let result = "";
+
+  //this first loop cycles through the indices of String s.
+  for(let sIndex = 0; sIndex < s.length; sIndex++){
+    //this second loop figures out how many times the letter should be repeated.
+    for(let letterCount = 0; letterCount <= sIndex; letterCount++){
+      //If this is the first letter in the sequence, then make it uppercase.
+      //otherwise, make it lowercase. statement is repeated until the
+      //letter has as many copies as it's supposed to.
+      if(letterCount === 0){
+        result = result + s[sIndex].toUpperCase();
+        } else {
+        result = result + s[sIndex].toLowerCase();
+        }
+      }
+    }
+  }
+  return result;
+}
